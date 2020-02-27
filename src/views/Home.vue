c12c1dc846e5f66e4464400cf212461e74d8afa2
Acllex <986169823@qq.com>
2020-02-27 12:26:26 +0800

    'first'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,288 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bodyer">
+    <button type="text" v-on:keydown="active">
+      <div class="big-box">
+        <div
+          :class="['box', datas[index]==0?'':'box1',datas[index]<=2?'':'box2', datas[index]<=4?'':'box3', datas[index]<=8?'':'box4', datas[index]<=16?'':'box5', datas[index]<=32?'':'box6']"
+          v-for="(item, index) in boxs"
+          :key="index"
+        >{{datas[index]==0?'':datas[index]}}</div>
+      </div>
+    </button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.bodyer {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+  background-color: #faf8ef;
 }
+.big-box {
+  width: 500px;
+  height: 500px;
+  background-color: #bbada0;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+  padding: 14px;
+  box-sizing: border-box;
+  border-radius: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.box {
+  width: 106px;
+  height: 106px;
+  background-color: rgba(238, 228, 218, 0.35);
+  border-radius: 6px;
+  margin: 4px 6px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 60px;
+  font-weight: 700;
+}
+.box1 {
+  background-color: #eee4da;
+
+  color: white;
+}
+.box2 {
+  background-color: #ede0c8;
+
+  color: white;
+}
+.box3 {
+  background-color: #f2b179;
+
+  color: white;
+}
+.box4 {
+  background-color: #f59563;
+  color: white;
+}
+.box5 {
+  background-color: #f67c5f;
+  color: white;
+}
+.box6 {
+  background-color: #f65e3b;
+  color: white;
+}
+button{
+  padding: 0;
+    border-width: 0;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      boxs: 16,
+      classname: "minbox",
+      datas: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      data: []
+    };
+  },
+  methods: {
+    setbox() {
+      let index = Math.floor(Math.random() * 16);
+      while (this.datas[index] != 0) {
+        index = Math.floor(Math.random() * 16);
+      }
+      let num = Math.floor(Math.random() * 3 + 2);
+      while (num == 3) {
+        num = Math.floor(Math.random() * 3 + 2);
+      }
+      this.$set(this.datas, index, num);
+    },
+    active() {
+      console.log(event.keyCode);
+      this.data = [...this.datas];
+
+      if (!this.datas) {
+        return;
+      }
+      if (event.keyCode == 38) {
+        this.datas.forEach((element, index) => {
+          let x = index;
+          while (x >= 4) {
+            if (this.datas[x - 4] != 0 && this.datas[x - 4] != element) {
+              break;
+            }
+            this.$set(this.datas, x, 0);
+            if (element == this.datas[x - 4]) {
+              this.$set(this.datas, x - 4, element + this.datas[x - 4]);
+            } else {
+              this.$set(this.datas, x - 4, element);
+            }
+            x = x - 4;
+          }
+        });
+      }
+      if (event.keyCode == 40) {
+        for (let i = this.datas.length; i >= 0; i--) {
+          let x = i;
+          while (x <= 11) {
+            if (this.datas[x + 4] != 0 && this.datas[x + 4] != this.datas[x]) {
+              break;
+            }
+
+            if (this.datas[x] == this.datas[x + 4]) {
+              this.$set(this.datas, x + 4, this.datas[x] + this.datas[x + 4]);
+              this.$set(this.datas, x, 0);
+              break;
+            } else {
+              this.$set(this.datas, x + 4, this.datas[x]);
+              this.$set(this.datas, x, 0);
+            }
+            x = x + 4;
+          }
+        }
+      }
+      if (event.keyCode == 37) {
+        this.datas.forEach((element, index) => {
+          let x = index;
+          if (x > 0 && x < 4) {
+            while (x > 0) {
+              if (this.datas[x - 1] != 0 && this.datas[x - 1] != element) {
+                break;
+              }
+              this.$set(this.datas, x, 0);
+              if (element == this.datas[x - 1]) {
+                this.$set(this.datas, x - 1, element + this.datas[x - 1]);
+              } else {
+                this.$set(this.datas, x - 1, element);
+              }
+              x = x - 1;
+            }
+          }
+          if (x > 4 && x < 8) {
+            while (x > 4) {
+              if (this.datas[x - 1] != 0 && this.datas[x - 1] != element) {
+                break;
+              }
+              this.$set(this.datas, x, 0);
+              if (element == this.datas[x - 1]) {
+                this.$set(this.datas, x - 1, element + this.datas[x - 1]);
+              } else {
+                this.$set(this.datas, x - 1, element);
+              }
+              x = x - 1;
+            }
+          }
+          if (x > 8 && x < 12) {
+            while (x > 8) {
+              if (this.datas[x - 1] != 0 && this.datas[x - 1] != element) {
+                break;
+              }
+              this.$set(this.datas, x, 0);
+              if (element == this.datas[x - 1]) {
+                this.$set(this.datas, x - 1, element + this.datas[x - 1]);
+              } else {
+                this.$set(this.datas, x - 1, element);
+              }
+              x = x - 1;
+            }
+          }
+          if (x > 12 && x < 16) {
+            while (x > 12) {
+              if (this.datas[x - 1] != 0 && this.datas[x - 1] != element) {
+                break;
+              }
+              this.$set(this.datas, x, 0);
+              if (element == this.datas[x - 1]) {
+                this.$set(this.datas, x - 1, element + this.datas[x - 1]);
+              } else {
+                this.$set(this.datas, x - 1, element);
+              }
+              x = x - 1;
+            }
+          }
+        });
+      }
+      if (event.keyCode == 39) {
+        for (let i = this.datas.length; i >= 0; i--) {
+          const element = this.datas[i];
+          let x = i;
+          if (x >= 0 && x < 4) {
+            while (x < 3) {
+              if (this.datas[x + 1] != 0 && this.datas[x + 1] != element) {
+                break;
+              }
+              this.$set(this.datas, x, 0);
+              if (element == this.datas[x + 1]) {
+                this.$set(this.datas, x + 1, element + this.datas[x + 1]);
+              } else {
+                this.$set(this.datas, x + 1, element);
+              }
+              x = x + 1;
+            }
+          }
+          if (x >= 4 && x < 8) {
+            while (x < 7) {
+              if (this.datas[x + 1] != 0 && this.datas[x + 1] != element) {
+                break;
+              }
+              this.$set(this.datas, x, 0);
+              if (element == this.datas[x + 1]) {
+                this.$set(this.datas, x + 1, element + this.datas[x + 1]);
+              } else {
+                this.$set(this.datas, x + 1, element);
+              }
+              x = x + 1;
+            }
+          }
+          if (x >= 8 && x < 11) {
+            while (x < 11) {
+              if (this.datas[x + 1] != 0 && this.datas[x + 1] != element) {
+                break;
+              }
+              this.$set(this.datas, x, 0);
+              if (element == this.datas[x + 1]) {
+                this.$set(this.datas, x + 1, element + this.datas[x + 1]);
+              } else {
+                this.$set(this.datas, x + 1, element);
+              }
+              x = x + 1;
+            }
+          }
+          if (x >= 12 && x < 16) {
+            while (x < 15) {
+              if (this.datas[x + 1] != 0 && this.datas[x + 1] != element) {
+                break;
+              }
+              this.$set(this.datas, x, 0);
+              if (element == this.datas[x + 1]) {
+                this.$set(this.datas, x + 1, element + this.datas[x + 1]);
+              } else {
+                this.$set(this.datas, x + 1, element);
+              }
+              x = x + 1;
+            }
+          }
+        }
+      }
+
+      let res = this.datas.every((ele, i) => ele == this.data[i]);
+      if (res) {
+        return;
+      } else {
+        this.setbox();
+      }
+    }
+  },
+  mounted() {
+    this.setbox();
+  }
+};
 </script>
